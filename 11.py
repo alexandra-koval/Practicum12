683277fc52e49af705288777a355b01f168feff2
koval.alexandra <koval.sashka07@gmail.com>
2026-05-16 12:15:53 +0700

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,9 +1,13 @@
 def ind_maxlist(a):
-    """
-    Returns the index of the maximum element in a list of integers
-    recursively.
-    """
-    if a[len(a) - 1] == max(a):
-        return len(a) - 1
+    if a == []:
+        return None
+    if len(a) == 1:
+        return 0
+    
+    max_index_rest = ind_maxlist(a[1:])
+    max_index_rest += 1
+    
+    if a[0] >= a[max_index_rest]:
+        return 0
     else:
-        return ind_maxlist(a[:-1])
+        return max_index_rest
